2713a087f79a067b3a7ad61b7a14e30cad165a2f
keji <15700085542@163.com>
2020-05-24 22:41:30 +0800

    week 01 homework

--- Week_01/[1]两数之和.java
+++ Week_01/[1]两数之和.java
@@ -17,7 +17,15 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int left = target - nums[i];
+            if(map.containsKey(left)){
+                return new int[]{i, map.get(left)};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
