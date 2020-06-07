2c4a8b9a0c8294f4e8fad6df0e0c74f244bec648
keji <15700085542@163.com>
2020-06-07 23:25:34 +0800

    week 03 homework

---
+++ Week_03/[169]多数元素.java
@@ -0,0 +1,34 @@
+//给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。 
+//
+// 你可以假设数组是非空的，并且给定的数组总是存在多数元素。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: [3,2,3]
+//输出: 3 
+//
+// 示例 2: 
+//
+// 输入: [2,2,1,1,1,2,2]
+//输出: 2
+// 
+// Related Topics 位运算 数组 分治算法
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int majorityElement(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        for(int i:nums){
+            if(stack. empty()||i==stack.peek()){
+                stack.push(i);
+            }else{
+                stack.pop();
+            }
+        }
+        return stack.peek();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
